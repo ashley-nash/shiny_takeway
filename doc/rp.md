12f0b3a88550f9525a35c1a938334d00bbfc6028
ashley-nash <sunhe1509@gmail.com>
2020-03-08 10:04:36 +0800

    add commit

---
+++ doc/rp.md
@@ -0,0 +1,5 @@
+# shiny_takeway
+
+这个项目依赖基站数据，主要是展示基站的到达出发人数。
+
+使用了tidyverse, shinythemes, plotly and shinyjs 这几个包的功能。
--- xb.R
+++ xb.R
@@ -1,135 +1,119 @@
 library(shiny)
+library(shinythemes)
 library(tidyverse)
 library(plotly)
 library(shinyjs)
+library(DT)
+library(leaflet)
 
+#读取数据
 db_sale <- read_csv("data.csv")
 db_geo <- read_csv("GeoLabel.csv")
+#格式化日期
+db_sale$日期 <- as.Date(as.character(db_sale$日期),"%Y%m%d")
+#拼接经纬度成新表
+db_sale=db_sale %>% left_join(db_geo , by=c("网格编号"="label"))
 
-gapminder %<>% mutate_at(c("year", "country"), as.factor)
+# 列名称要格式成英文
+colnames(db_sale) <- c('day','t_time','dat','zlnum','cfnum','ddnum','lng','lat')
 
-gapminder_years <- levels(gapminder$year)
-gapminder_countries <- levels(gapminder$country) %>% str_sort()
 
-headerRow <- div(id="header", useShinyjs(),
-                selectInput("selYear", 
-                         label="Select the Year", 
-                         multiple = TRUE,
-                         choices=gapminder_years,
-                         selected=head(gapminder_years,3)),
-                selectInput("selCountry", 
-                            label="Select the Coutry", 
-                            multiple = TRUE,
-                            choices=gapminder_countries,
-                            selected=head(gapminder_countries,2))
+
+
+mapPanel <- tabPanel("cfnumPanel",                     
+					fluidRow(
+					  #--选择日期按钮
+					  column(width=4,selectInput("selecta", label = h3("日期选择"), choices = as.character(unique(db_sale$day)), selected = '2017-09-01')),
+					  #--选择时间点按钮
+					  column(width=4,selectInput("selectb", label = h3("时刻选择"), choices = as.character(unique(db_sale$t_time)), selected = '0'))
+					 ),
+					h4('各站点每小时的出发流量'), 
+					fluidRow(
+					  #--地图1
+					  column(width=12,leafletOutput("cfnum"))
+					 ),
+					h4('各站点每小时的到达流量'),					 
+					fluidRow(
+					  #--地图2
+					  column(width=12,leafletOutput("ddnum"))
+					 )					 
+							  )
+plot_ly <- tabPanel("plot_ly",                     
+					fluidRow(
+					  #--选择日期按钮
+					  column(width=4,selectInput("selectc", label = h3("日期选择"), choices = as.character(unique(db_sale$day)), selected = '2017-09-01')),
+					  #--选择站点按钮
+					  column(width=4,selectInput("selectd", label = h3("基站选择"), choices = as.character(unique(db_sale$dat)), selected = '13'))
+					 ),
+					h4('各站点每天流量变化'), 
+					fluidRow(
+					  column(width=12,plotlyOutput(outputId = "ptnum"))
+					 )					 
+							  )
+
+dataPanel <- tabPanel("dataPanel",                     
+					fluidRow(
+					  #--选择日期按钮
+					  column(width=4,selectInput("selecte", label = h3("日期选择"), choices = as.character(unique(db_sale$day)), selected = '2017-09-01')),
+					  #--选择站点按钮
+					  column(width=4,selectInput("selectf", label = h3("基站选择"), choices = as.character(unique(db_sale$dat)), selected = '13'))
+					 ),
+					h4('根据条件下载需要的数据结果'), 
+					fluidRow(
+					  column(width=12,dataTableOutput('datanum'))
+					 )					 
+							  )
+
+## UI端
+ui <- navbarPage("Talk Way",theme = shinythemes::shinytheme("united"),
+                 mapPanel    #地图     -- 筛选框选择日期 和 时间点
+				,plot_ly
+				,dataPanel
 )
 
-dataPanel <- tabPanel("Data",
-                      tableOutput("dataTable")
-             )
-
-plotPanel <- tabPanel("Plot",
-                      fluidRow(
-                        column(width = 8,
-                               plotOutput("plotData",
-                                          hover = hoverOpts(id = "plot_hover", delayType = "throttle"))
-                        ),
-                        column(width = 4,
-                               h2("Info"),
-                               div("Country: ",
-                                   textOutput("txtCountry", inline = TRUE)
-                               ),
-                               div("Year: ",
-                                   textOutput("txtYear", inline = TRUE)
-                               ),
-                               div("Population: ",
-                                   textOutput("txtPop", inline = TRUE)
-                               )
-                               # verbatimTextOutput("plot_hoverinfo")
-                        )
-                      ) # fluidRow
-          ) # tabPanel
-
-plotlyPanel <- tabPanel("Plotly",
-                        plotly::plotlyOutput("plotlyData")
-            )
-
-mapPanel <- tabPanel("Map",
-                     tableOutput("mapTable")
-)
-
-ui <- navbarPage("take-away",
-                 dataPanel,
-                 plotPanel,
-                 plotlyPanel,
-                 mapPanel,
-                 id = "navBar",
-                 header = headerRow
-)
-
+## 服务器端
 server <- function(input, output) {
+# 地图
+output$cfnum <- renderLeaflet({
+	      tb=filter(db_sale ,day == input$selecta & t_time== input$selectb ) #tb=filter(db_sale ,day == '2017-09-01' & t_time=='0' )	  
+		  leaflet(data = tb) %>% addTiles() %>%
+		  addMarkers(~lng, ~lat, popup = ~as.character(cfnum), label = ~as.character(cfnum))  
+  }) 
   
-  observe(if(input$navBar=="Map") {
-    cat(file=stderr(), input$navBar, "\n")
-    shinyjs::hide("header")
-  } else {
-    cat(file=stderr(), input$navBar, "\n")
-    shinyjs::show("header")
-  })
 
-  gapminder_filtered <- reactive({
-    req(input$selCountry)
-    req(input$selYear)
-    gapminder %>% filter(year %in% input$selYear, 
-                         country %in% input$selCountry)
-    })
+output$ddnum <- renderLeaflet({
+	      tb=filter(db_sale ,day == input$selecta & t_time== input$selectb ) #tb=filter(db_sale ,day == '2017-09-01' & t_time=='0' )	  
+		  leaflet(data = tb) %>% addTiles() %>%
+		  addMarkers(~lng, ~lat, popup = ~as.character(ddnum), label = ~as.character(ddnum))  
+  }) 
   
-  output$dataTable <- renderTable({
-    gapminder_filtered()
-  })
-  
-  output$mapTable <- renderTable({
-    head(esDB)
-  })
-  
-  output$plotData <- renderPlot({
-    gapminder_filtered() %>%
-      ggplot(aes(x=country, y=pop, fill=year)) +
-      geom_bar(stat="identity", position=position_dodge())
-  })
-  
-  output$plotlyData <- plotly::renderPlotly({
-    gapminder_filtered() %>%
-      ggplot(aes(x=country, y=pop, fill=year)) +
-      geom_bar(stat="identity", position=position_dodge())
-  })
-  
-#  output$plot_hoverinfo <- renderPrint({
-#    cat("Hover (throttled):\n")
-#    str(input$plot_hover)
-#  })
-  
-  countryIndex <- reactive({
-    req(input$plot_hover$x)
-    round(input$plot_hover$x)
-    })
-  countryName <- reactive({
-    req(countryIndex() > 0 & countryIndex() <= length(input$selCountry))
-    str_sort(input$selCountry)[countryIndex()]
-    })
-  output$txtCountry <- renderText(countryName())
-  yearIndex <- reactive({
-    req(input$plot_hover$x)
-    ceiling((input$plot_hover$x - round(input$plot_hover$x) + 0.5) * length(input$selYear))
-  })
-  yearName <- reactive({
-    req(yearIndex() > 0 & yearIndex() <= length(input$selYear))
-    str_sort(input$selYear)[yearIndex()]
-  })
-  output$txtYear <- renderText(yearName())
-  output$txtPop <- renderText(
-    gapminder_filtered() %>% filter(year == yearName(), country == countryName()) %>% pull(pop)
-    )
+##画走势图(选择日期，显示24小时走势)
+output$ptnum <- renderPlotly({
+		tx= filter(db_sale ,day == input$selectc & dat== input$selectd )
+		p <- plot_ly(data=tx, x = ~t_time) %>%
+			add_lines(y = ~zlnum, name = "驻留人数") %>%
+			add_lines(y = ~cfnum, name = "出发人数") %>%
+			add_lines(y = ~ddnum, name = "到达人数") %>%	
+			layout(
+			  title = "各网格人数变化",
+			  xaxis = list(title = "小时数"),
+			  yaxis = list(title = "人口流量数"))  
+			  
+		p
+					 })
+
+## 数据表下载 pdf格式
+
+output$datanum <- DT::renderDataTable({
+                    tx= filter(db_sale ,day == input$selecte & dat== input$selectf )
+					datatable(
+					  tx, extensions = 'Buttons', options = list(
+						dom = 'Bfrtip',
+						buttons = c('pdf', 'print')
+					  )
+					)									
+					 })
+
 }
 
 # Run the application 
